c708b21ea7405c9bc83efd53e10f06bb9c31d7ab
rhacnddl <jiho519@naver.com>
2021-09-27 18:50:20 +0900

    일부러 예외 발생

---
+++ src/main/java/com/example/jenkinstest/api/TestApiController.java
@@ -0,0 +1,28 @@
+package com.example.jenkinstest.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tests")
+public class TestApiController {
+
+    @GetMapping("/hello")
+    public HelloResponse getHello(){
+        return new HelloResponse(1L, "Hello World");
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class HelloResponse{
+        private Long id;
+        private String content;
+    }
+}
