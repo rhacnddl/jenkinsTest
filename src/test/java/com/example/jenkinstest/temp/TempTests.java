abeb50c932e3524b99b469a9c8d490aefe32eedd
rhacnddl <jiho519@naver.com>
2021-09-29 16:10:27 +0900

    new world v5

--- src/test/java/com/example/jenkinstest/temp/TempTests.java
+++ src/test/java/com/example/jenkinstest/temp/TempTests.java
@@ -15,6 +15,7 @@ public class TempTests {
         System.out.println("hello new world v2");
         System.out.println("hello new world v3");
         System.out.println("hello new world v4");
+        System.out.println("hello new world v5");
 
         //String str = list.get(0);
 
