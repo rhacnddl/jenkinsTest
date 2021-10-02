6fd8ba910f64274eabb63ef85a218a720add8a59
rhacnddl <jiho519@naver.com>
2021-10-02 15:45:31 +0900

    new world v14

--- src/main/java/com/example/jenkinstest/api/TestApiController.java
+++ src/main/java/com/example/jenkinstest/api/TestApiController.java
@@ -14,7 +14,7 @@ public class TestApiController {
 
     @GetMapping("/hello")
     public HelloResponse getHello(){
-        return new HelloResponse(1L, "Hello World V3");
+        return new HelloResponse(1L, "Hello World V4");
     }
 
     @Getter
